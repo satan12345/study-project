33b5760afdc0932f28ebe0663a84360acdac6a48
jipeng <jipeng@able-elec.com>
2020-04-29 13:26:09 +0800

    编写bio chatserver与chathandler代码

--- src/main/java/com/able/studyproject/io/second/ChatHandler.java
+++ src/main/java/com/able/studyproject/io/second/ChatHandler.java
@@ -1,10 +1,58 @@
 package com.able.studyproject.io.second;
 
+import org.springframework.util.StringUtils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
 /**
  * @param
  * @author jipeng
  * @date 2020-04-28 19:05
  */
-public class ChatHandler {
+public class ChatHandler implements Runnable {
+    private ChatServer chatServer;
+    private Socket socket;
+
+    public ChatHandler(ChatServer chatServer, Socket socket) {
+        this.chatServer = chatServer;
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                //将客户端添加到服务器中
+                chatServer.addClient(socket);
+                //读取用户发送的消息
+                BufferedReader bufferedReader=new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                String s = bufferedReader.readLine();
+                System.out.println("客户端【"+socket.getPort()+"】:"+s);
+                //用户退出
+                if (!StringUtils.isEmpty(s)&& ChatServer.QUIT.equals(s)) {
+                    chatServer.removeClient(socket);
+                    socket=null;
+                    break;
+                }
+                //将消息转发给聊天室中的其他在线用户
+                chatServer.boardcastMsg(socket,s+"\n");
+
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            if (socket!=null) {
+                try {
+                    chatServer.removeClient(socket);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
 
--- src/main/java/com/able/studyproject/io/second/ChatServer.java
+++ src/main/java/com/able/studyproject/io/second/ChatServer.java
@@ -1,5 +1,7 @@
 package com.able.studyproject.io.second;
 
+import org.springframework.util.StringUtils;
+
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
@@ -8,6 +10,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * @param
@@ -16,26 +19,93 @@ import java.util.Map;
  */
 public class ChatServer {
     /**
-    *
-    */
-    private static final Integer DEFAULT_PORT=8888;
-    private final String QUIT="quit";
+     *
+     */
+    private static final Integer DEFAULT_PORT = 8888;
+    public static final String QUIT = "quit";
 
-    private  ServerSocket serverSocket;
+    private ServerSocket serverSocket;
 
     private Map<Integer, Writer> connectecdClient;
 
-    public ChatServer() throws IOException {
-        serverSocket=new ServerSocket(DEFAULT_PORT);
-        connectecdClient=new HashMap<>();
+
+    public void start() {
+        try {
+            serverSocket = new ServerSocket(DEFAULT_PORT);
+
+            System.out.println("服务器已经启动,正在监听端口:" + DEFAULT_PORT);
+
+            while (true) {
+                //等待客户端连接
+                Socket socket = serverSocket.accept();
+                //@TODO 创建ChatHandler线程 用于连接的客户端事件处理
+                new Thread(new ChatHandler(this,socket)).start();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            close();
+        }
+    }
+
+    public void close(){
+        if (serverSocket==null) {
+            return;
+        }
+        try {
+            serverSocket.close();
+            System.out.println("serverSocket已经关闭");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public ChatServer() {
+        connectecdClient = new ConcurrentHashMap<>();
+
     }
 
     public void addClient(Socket socket) throws IOException {
-        if (socket==null) {
+        if (socket == null) {
             return;
         }
-        BufferedWriter bufferedWriter=new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-        connectecdClient.put(socket.getPort(),bufferedWriter);
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        connectecdClient.put(socket.getPort(), bufferedWriter);
+    }
+
+    public void removeClient(Socket socket) throws IOException {
+        if (socket == null) {
+            return;
+        }
+
+        int port = socket.getPort();
+        Writer writer = connectecdClient.get(port);
+        if (writer != null) {
+            writer.close();
+            connectecdClient.remove(port);
+            System.out.println("客户端【" + port + "】已经断开连接了");
+        }
+        boardcastMsg(socket,"客户端【" + port + "】已经退出");
+
+    }
+
+    public void boardcastMsg(Socket socket, String msg) {
+        if (socket == null || StringUtils.isEmpty(msg)) {
+            return;
+        }
+        int port = socket.getPort();
+        connectecdClient.forEach((k, v) -> {
+            if (!k.equals(port)) {
+                try {
+                    v.write("来自" + port + "的消息" + msg);
+                    v.flush();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
     }
 
 }
